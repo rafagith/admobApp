7fc2dcb6b29b9c169c8bc6835e0e68bbc662657f
Rafael-Rosales <rrosalesb90@gmail.com>
2019-10-01 15:50:40 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:admob_app/src/home_page.dart';
+import 'package:admob_flutter/admob_flutter.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  Admob.initialize("com.example.admob_app");
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Admob Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(title: 'Home Page'),
+    );
+  }
+}
